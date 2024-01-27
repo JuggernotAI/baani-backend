4553eb87323180ea557f84b1acb4d9d90cb6489b
PraX03 <pratham10157@gmail.com>
2024-01-27 11:40:54 +0530

    FIX: Image DIR issue

--- image/dalle_handler.py
+++ image/dalle_handler.py
@@ -1,14 +1,27 @@
 import os
 import requests
-
+from urllib.parse import urlparse
+from datetime import datetime
+temp_dir = "temp"
 def dalle_handler(url=None):
-    file_path="temp/"
     if url:
-        response = requests.get(url)
-        if response.status_code == 200:
-        # Write the image to a file
-            with open(file_path, 'wb') as file:
-                file.write(response.content)
-            print(f"Image saved to {file_path}")
-        else:
-            print("Failed to download the image")
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                # Parse the URL to get the image name or use a timestamp
+                image_name = os.path.basename(urlparse(url).path)
+                if not image_name:
+                    # If the URL does not contain an image name, use a timestamp
+                    image_name = datetime.now().strftime("%Y%m%d%H%M%S") + ".jpg"
+                
+                # Complete file path
+                file_path = os.path.join(temp_dir, image_name)
+
+                # Write the image to a file
+                with open(file_path, 'wb') as file:
+                    file.write(response.content)
+                print(f"Image saved to {file_path}")
+            else:
+                print("Failed to download the image: Status code", response.status_code)
+        except requests.RequestException as e:
+            print("Error while downloading the image:", e)
