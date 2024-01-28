88553375ba1d94ab740cce84a4fe94f4e672cc71
JuggernotAI <149756202+JuggernotAI@users.noreply.github.com>
2024-01-28 20:39:25 +0530

    ft: remove upload image upon generation

--- image/dalle_handler.py
+++ image/dalle_handler.py
@@ -5,23 +5,25 @@ from datetime import datetime
 temp_dir = "temp"
 def dalle_handler(url=None):
     if url:
-        try:
-            response = requests.get(url)
-            if response.status_code == 200:
-                # Parse the URL to get the image name or use a timestamp
-                image_name = os.path.basename(urlparse(url).path)
-                if not image_name:
-                    # If the URL does not contain an image name, use a timestamp
-                    image_name = datetime.now().strftime("%Y%m%d%H%M%S") + ".jpg"
+        pass
+        # try:
+        #     print("this is your url", url)
+        #     response = requests.get(url)
+        #     if response.status_code == 200:
+        #         # Parse the URL to get the image name or use a timestamp
+        #         image_name = os.path.basename(urlparse(url).path)
+        #         if not image_name:
+        #             # If the URL does not contain an image name, use a timestamp
+        #             image_name = datetime.now().strftime("%Y%m%d%H%M%S") + ".jpg"
                 
-                # Complete file path
-                file_path = os.path.join(temp_dir, image_name)
+        #         # Complete file path
+        #         file_path = os.path.join(temp_dir, image_name)
 
-                # Write the image to a file
-                with open(file_path, 'wb') as file:
-                    file.write(response.content)
-                print(f"Image saved to {file_path}")
-            else:
-                print("Failed to download the image: Status code", response.status_code)
-        except requests.RequestException as e:
-            print("Error while downloading the image:", e)
+        #         # Write the image to a file
+        #         with open(file_path, 'wb') as file:
+        #             file.write(response.content)
+        #         print(f"Image saved to {file_path}")
+        #     else:
+        #         print("Failed to download the image: Status code", response.status_code)
+        # except requests.RequestException as e:
+        #     print("Error while downloading the image:", e)
